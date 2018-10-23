b9739bbe387bf26ad81c6db70d810b12229c2fb9
LeslieGayer77 <42385458+LeslieGayer77@users.noreply.github.com>
2018-10-23 09:46:35 -0700

    Math and Comments

--- notes/venv/hello world.py
+++ notes/venv/hello world.py
@@ -1,1 +1,31 @@
 print("Hello world")
+
+
+#this is a comment
+#1. make notes to myself
+#2. Comment pieces of code that do not work
+#3. make my code easier to read
+
+
+print ("Look at what happens her. Is there any space?")
+print()
+print()
+print ("There should be a couple blank lines here.")
+
+#math
+print (3+5)
+print (5-2)
+print (3*4)
+print (6/2)
+
+print("Figure this out...")
+print (6//2)
+print (5//2)
+print (9//4)
+# // = take off the extra part after the decimal
+
+print ("here is another one")
+print (6%2)
+print (5%2)
+print (11%4)
+# modulus (Remainder)
