627c6da1a4644283d626b92d2dd6a8c036e51d82
LeslieGayer77 <42385458+LeslieGayer77@users.noreply.github.com>
2018-10-24 09:46:07 -0700

    Added Basic notes for madlibs

--- notes/venv/hello world.py
+++ notes/venv/hello world.py
@@ -29,3 +29,31 @@ print (6%2)
 print (5%2)
 print (11%4)
 # modulus (Remainder)
+
+#Powers
+# What is 2^20
+print(2**4)
+print(3**27)
+
+#Taking input
+name = input ("What is your name?")
+print("Hello %s" % name)
+
+age = input ("How old are you? >_")
+print ("%s?!? You belong in a museum." % age)
+print()
+print("%s IS REALLY OLD. She IS %s YEARS OLD." % (name, age))
+
+# Variable Assignments
+car_name = "Baby"
+car_type = "1967 Chevy Impala Sport Sedan"
+car_cylinders = 16
+car_miles_per_gallon = 0.01
+
+# Make it print "I have a car called Baby. It is a tesla."
+print("I have a car called %s. It is a %s." % (car_name, car_type))
+
+#Recasting
+real_age = int(input("How old are you again?"))
+hidden_age = real_age + 5
+print("This is your real age: %d" % hidden_age)
