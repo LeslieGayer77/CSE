0292cc14d09bbd853476e5125d654fe5bd851db4
LeslieGayer77 <42385458+LeslieGayer77@users.noreply.github.com>
2018-11-09 10:39:54 -0800

    finishing up on lucky 7s

--- notes/venv/Lucky 7's.py
+++ notes/venv/Lucky 7's.py
@@ -3,20 +3,25 @@ import random
 money = 15
 dice1 = 0
 dice2 = 0
-lose = False
+lose = True
+round = 0
+highscore = 0
+best_round = 0
 while money > 0:
-    num = 1
+    num = int(input("What are you betting?"))
     money = money - num
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
     if dice1 + dice2 == 7:
         print("You got 7!")
         print("+ %s" % num)
+        money = money + num + 4
     elif not dice1 + dice2 == 7:
-        print("Sorry - %s" % num)
-    elif money == 0:
-        lose = True
+        print("Sorry you rolled %s and %s" % (dice1, dice2,))
+        print("- %s" % num)
+    print("You have %s dollars left" % money)
+    print()
 
 print()
-if money == 0:
+if lose == True:
     print("You dont have anymore money")
