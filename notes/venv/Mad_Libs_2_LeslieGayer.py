465bc25db67feb18601b1ecc52d7228331d75615
LeslieGayer77 <42385458+LeslieGayer77@users.noreply.github.com>
2018-11-28 09:25:31 -0800

    Mad LIbs2

---
+++ notes/venv/Mad_Libs_2_LeslieGayer.py
@@ -0,0 +1,22 @@
+#Mad Lib
+"""holiday = input ("Whats a holiday? ")
+verb = input ("What's a verb with ing? ")
+fruit = input ("What's a fruit? ")
+person = input ("What's a persons name? ")
+thing = input ("What's a thing? ")
+thing2 = input ("What's another thing? ")
+feeling = input ("Name a feeling. ")
+food = input ("List a food item. ")
+drink = input ("list a drink item. ")
+verb2 = input ("List another verb. ")
+person2 = input ("List another person. ")
+thing3 = input ("List another thing ")"""
+woop = ["Holloween", "cut", "pumpkins", "Brihanna", "wax", "shoe", "sad", "crackers", "kool-aid", "throw"
+        "rocks", "Sydney", "camera"]
+print("We are so excited it's %s!" % woop[0])
+print("After we %s %s with my friend," % (woop[1],woop[2]))
+print ("%s, we're going on a %s ride and through a %s Maze" % (woop[3],woop[4],woop[5]))
+print ("We're going to be %s after, so dinner is next!" % woop[6])
+print ("We are going to eat %s and wash it down with %s." % (woop[7],woop[8]))
+print ("Late at night, we plan to %s down a scary street." % woop[9])
+print ("%s says it's haunted! But we wont be scared since we have a %s. Happy Holloween!" % (woop[10],woop[11]))
