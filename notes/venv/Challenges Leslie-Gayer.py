d639ff6fa73cd79ab6143eba0fe0704c1b8148d8
LeslieGayer77 <42385458+LeslieGayer77@users.noreply.github.com>
2018-12-04 09:45:56 -0800

    finishing5 challenge

--- notes/venv/Challenges Leslie-Gayer.py
+++ notes/venv/Challenges Leslie-Gayer.py
@@ -1,20 +1,48 @@
+import math
 #1
 def challenge1 (Firstname, lastname):
     print(lastname, Firstname)
-
-
 challenge1("Leslie", "Gayer")
+print()
 
 #2
-
-
 def challenge2(num):
-
-
-    if nam > 0:
-        print("This is odd")
+    factor = 0
+    product = 0
+    while factor < num and not product == num:
+        factor = factor + 1
+        product = factor * 2
+    if product == num:
+        return "$s is even" % num
     else:
-        print("This is even")
+        return "%s is not even" % num
+print(challenge2(3))
+print()
+
+#3
+def challenge3(b, h):
+    return(b*h/2)
+print(challenge3(3,4))
+print()
+
+#4
+def challenge4(numm):
+    if numm == 0:
+        print("This number is zero")
+    elif numm > 0:
+        print("This number is positive")
+    else:
+        print("This number is negative")
+print(challenge4(-3))
+print()
+
+#5
+def challenge5(r):
+    math.pi
+    V = 4.0 / 3.0 * pi * r ** 3
+    return V
+print(challenge5(6))
 
 
-challenge2(2)
+
+
--- notes/venv/Leslie Gayer - Guess Game.py
+++ notes/venv/Leslie Gayer - Guess Game.py
@@ -5,20 +5,25 @@ number = random.randint(1, 100)
 guesses = 10
 win = False
 while guesses > 0:
-    num = int(input("What's a number from 1 to 10?"))
+    num = int(input("What's a number from 1 to 100?"))
+    print()
     if num >= 101:
         print("WAY TOO HIGH. Follow the directions")
         guesses = guesses - 1
+        print("You have %s guesses left" % guesses)
     elif num > number:
         print("a little lower")
         guesses = guesses - 1
+        print("You have %s guesses left" % guesses)
     elif num < number:
         print("a little higher")
         guesses = guesses - 1
+        print("You have %s guesses left" % guesses)
     elif num == number:
         print("Correct")
         guesses = 0
         win = True
+        print("You have %s guesses left" % guesses)
 print()
 if win:
     print("You Win!")
