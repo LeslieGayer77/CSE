fba87eafcc2f5506f557b175eebac6a34a4d4e65
LeslieGayer77 <42385458+LeslieGayer77@users.noreply.github.com>
2019-01-08 09:45:03 -0800

    Semester 2

---
+++ Semester 2 Notes.py
@@ -0,0 +1,27 @@
+print("Hello World")
+
+# Hello
+
+cars = 5
+driving = True
+
+print("I have %s Cars" % cars)
+print("I have " + str(cars) + " cars")
+
+# question = input("How old are you?")
+# print("Wow! %s?!?! ewwwwww" % question)
+# print(question + " Wow! %s?!?! ewwwwww")
+
+colors = ["Blue", "Red", "Burgundy", "Green", "Military"]
+
+colors.append("Purple")
+
+print(colors)
+
+colors.pop(0)
+
+print(colors)
+
+print(colors[2])
+
+print(len(colors))
