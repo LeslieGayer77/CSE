0092b75d2055e1b2de5126a62d870ef76612483f
LeslieGayer77 <42385458+LeslieGayer77@users.noreply.github.com>
2019-01-23 09:36:27 -0800

    more hangman

--- notes/Hangman-Leslie Gayer 2.py
+++ notes/Hangman-Leslie Gayer 2.py
@@ -1,12 +1,15 @@
 import random
 import string
 wordbank =["Supernatural", "Butterfly", "School is great!", "Pokemon", "Minecraft", "Avengers",
-            "Pizza!", "Captain America", "Iron Man"]
+            "Pizza!", "Captain America", "Iron Man", "Awkward", "Cupcake", "Promiscuous", "superstitious",
+           "Goldfish", "koolaid", "Reaper", "Burgundy", "Military"]
+
 win = False
 letters_guessed = []
 word = random.choice(wordbank)
 guesses = 8
 blank = []
+
 for i in range(len(word)):
     if word[i] in list(string.punctuation):
         blank.append(word[i])
@@ -15,9 +18,12 @@ for i in range(len(word)):
     else:
         blank.append("*")
 
-print(blank)
+print("Welcome to hangman")
+print("You have 8 guesses, Good Luck!")
+print()
 while guesses > 0 and not win:
     letter = input("What is a letter?")
+    print()
     if letter.lower() in letters_guessed:
         print("You already said that!")
     elif letter in word.lower():
@@ -27,7 +33,7 @@ while guesses > 0 and not win:
         print("Nope.")
         list.append(letters_guessed, letter)
         guesses = guesses - 1
-    print()
+
     letters_found = 0
     for i in range(len(word)):
         if word[i] in list(string.punctuation):
@@ -43,8 +49,13 @@ while guesses > 0 and not win:
     print(display)
     if display == word:
         win = True
-
+    print("You have %s guesses left" % guesses)
+    print("Letters Guessed %s" % letters_guessed)
+    print()
 if win:
     print("You won!")
+    print("The word is %s" % word)
 else:
     print("You lost.")
+    print("The word was %s" % word)
+print()
