f89dcae3644f9bd228a92981a714cb3c330cd5cb
LeslieGayer77 <42385458+LeslieGayer77@users.noreply.github.com>
2019-01-31 09:44:43 -0800

    Map

--- notes/Leslie.Gayer_Dictionary.World.Map.py
+++ notes/Leslie.Gayer_Dictionary.World.Map.py
@@ -2,7 +2,7 @@ world_map = {
     'R19A': {
         'NAME': "Mr. Weibe's room",
         'DESCRIPTION': "The classroom you are in right now"
-                                  "now. There are two doors on the North wall.",
+                                  "There are two doors on the North wall.",
         'PATHS': {
              'NORTH': "PARKING_LOT"
         }
@@ -18,10 +18,20 @@ world_map = {
 
 
 #Controller
+playing = True
 current_node = world_map["R19A"]
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
 while playing:
     print(current_node['NAME'])
     print(current_node['DESCRIPTION'])
     command = input(">_")
     if command.lower() in ['q', 'quit', 'exit']:
         playing = False
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't got that way")
+    else:
+        print("Command Not Found")
