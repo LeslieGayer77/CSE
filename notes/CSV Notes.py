88e955c0fa7e6e349f0820726a7d820446821313
LeslieGayer77 <42385458+LeslieGayer77@users.noreply.github.com>
2019-04-26 09:48:09 -0700

    435t453

--- notes/CSV Notes.py
+++ notes/CSV Notes.py
@@ -1,5 +1,27 @@
 import csv
 
+
+def validate(num: str):
+    if not all16digits(num):
+        return False
+    if devisableby3(num) and devisableby2(num):
+        return True
+    return False
+
+
+def devisableby3(num: str):
+    first_num = int(num[0])
+    if first_num % 3 == 0:
+        return True
+    return False
+
+
+def devisableby2(num: str):
+    first_num = int(num[0])
+    if first_num % 2 == 0:
+        return True
+    return False
+
 # with open("Book1.csv", 'r') as old_csv:
 #     with open("MyNewFile.csv", 'w', newline='') as new_csv:
 #         print("Writing file.....")
@@ -14,16 +36,34 @@ import csv
 #
 # print("OK")
 
+# with open("Book1.csv", 'r') as old_csv:
+#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
+#         print("Writing file.....")
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#
+#         for row in reader:
+#             # Old_number = int(row[0]) + 1
+#             old_number = row[0]
+#             first_num = int(old_number[0])
+#             if first_num % 2 == 0:
+#                 writer.writerow(row)
+#
+# print("OK")
+
+
 with open("Book1.csv", 'r') as old_csv:
     with open("MyNewFile.csv", 'w', newline='') as new_csv:
         print("Writing file.....")
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
+
         for row in reader:
             # Old_number = int(row[0]) + 1
             old_number = row[0]
             first_num = int(old_number[0])
-            if first_num %2 == 0:
-                writer.writerow(row)
 
-print("OK")
+
+def all16digits(num: str):
+    if len(num) == 16:
+        return True
