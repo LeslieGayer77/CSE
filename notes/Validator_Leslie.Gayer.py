7f5085e833ab900f600cf67cb0bdc99abfa45426
LeslieGayer77 <42385458+LeslieGayer77@users.noreply.github.com>
2019-05-02 09:43:58 -0700

    validatror

--- notes/Validator_Leslie.Gayer.py
+++ notes/Validator_Leslie.Gayer.py
@@ -7,20 +7,6 @@ import csv
 # (Modulo 10)
 
 
-with open("Book1.csv", 'r') as old_csv:
-    with open("MyNewFile.csv", 'w', newline='') as new_csv:
-        print("Writing file.....")
-        reader = csv.reader(old_csv)
-        writer = csv.writer(new_csv)
-
-        for row in reader:
-            # Old_number = int(row[0]) + 1
-            old_number = row[0]
-            first_num = int(old_number[0])
-            last_num = int(old_number[15])
-            num15 = 
-
-
 def all16digits(num: str):
     if len(num) == 16:
         print("All the digits are 16 numbers long")
@@ -28,22 +14,54 @@ def all16digits(num: str):
     return False
 
 
-def drop_last_digit:
-    last_num
+def drop_last_digit(num: str):
+    return num[:14]
 
 
 def reverse_it(string):
-    print(string[::-1])
+    return string[::-1]
+
+
+def addnums(nums: list):
+    sum_num = 0
+    for index in range(len(nums)):
+        sum_num += int(nums[index])
+    return sum_num
 
 
 def multiply(num):
-    list_num = list(number)
+    list_num = list(num)
     for index in range(len(list_num)):
-        list_num[index] = int(list_num[index])
-
+        if index % 2 == 0:
+            list_num[index] = int(list_num[index])
+            list_num[index] *= 2
+            if list_num[index] > 9:
+                list_num[index] -= 9
+    return addnums(list_num)
 
 
 def validate_card_number(num: str):
+    if not all16digits(num):
+        print("This number is not 16 digits. It is not valid.")
+        return False
+    if not multiply(reverse_it(drop_last_digit(num))) % 10 == num[15]:
+        print("This number is not valid")
+        return False
+    print("This number is valid")
+    return True
 
 
-print(all16digits("5431709304959590"))
+multiply(reverse_it(drop_last_digit("5431709304959590")))
+validate_card_number("5431709304959590")
+
+with open("Book1.csv", 'r') as old_csv:
+    with open("invalid_numbers.csv", 'w', newline='') as new_csv:
+        print("Writing file.....")
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        for row in reader:
+            # Old_number = int(row[0]) + 1
+            old_number = row[0]
+            if not validate_card_number(old_number):
+                print(old_number, "is not valid")
+                writer.writerow(row)
